0e5f89ed3c9f139cda59ba11d1549b89b8c085ba
Wais <wais.almakaleh@gmail.com>
2020-04-27 03:45:01 -0400

    until next time

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const apiRouter = require('./api-router.js');
+const authRouter = require('../auth/auth-router');
 const configureMiddleware = require('./configure-middleware.js');
 
 const server = express();
@@ -8,5 +9,6 @@ const server = express();
 configureMiddleware(server);
 
 server.use('/api', apiRouter);
+server.use('/api/auth', authRouter)
 
 module.exports = server;
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,13 +1,16 @@
+const bcrypt = require('bcryptjs')
 const router = require('express').Router();
 
 const Users = require('../users/users-model.js');
 
 router.post('/register', (req, res) => {
-  let user = req.body;
+  const user = req.body;
+  const hash = bcrypt.hashSync(user.password, 8);
+  user.password = hash;
 
   Users.add(user)
     .then(saved => {
-      res.status(201).json(saved);
+      res.status(201).json({saved});
     })
     .catch(error => {
       res.status(500).json(error);
@@ -15,12 +18,12 @@ router.post('/register', (req, res) => {
 });
 
 router.post('/login', (req, res) => {
-  let { username, password } = req.body;
+  const { username, password } = req.body;
 
   Users.findBy({ username })
     .first()
-    .then(user => {
-      if (user) {
+    .then(([user]) => {
+      if (user && bcrypt.compareSync(password, user.password)) {
         res.status(200).json({ message: `Welcome ${user.username}!` });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
