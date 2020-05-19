952c556db33cf1a51d2ce8cebeeef216f7faf8d6
Wais <wais.almakaleh@gmail.com>
2020-05-18 23:18:31 -0400

    adding notes

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,14 +1,15 @@
-const bcrypt = require('bcryptjs')
+const bcrypt = require('bcryptjs')// import bcryptjs
 const router = require('express').Router();
 
 const Users = require('../users/users-model.js');
 
 router.post('/register', (req, res) => {
-  const user = req.body;
-  const hash = bcrypt.hashSync(user.password, 8);
-  user.password = hash;
+  const user = req.body;// get the user from the body
+  const hash = bcrypt.hashSync(user.password, 8); // hash using bycrypt modle
+  // the "8" means we are hashing the password 2 to the 8th times which is 256 times its hashing the password and continueing hashing the hash
+  user.password = hash; // change the user password into a hash
 
-  Users.add(user)
+  Users.add(user) // add method
     .then(saved => {
       res.status(201).json({saved});
     })
@@ -18,7 +19,7 @@ router.post('/register', (req, res) => {
 });
 
 router.post('/login', (req, res) => {
-  const { username, password } = req.body;
+  const { username, password } = req.body; // destruct the username and password fromt the body
 
   Users.findBy({ username })
     .first()
--- notes.md
+++ notes.md
@@ -1,1 +1,16 @@
 # Authentication Notes
+
+data is protected by something someone knows, something they have or something they are
+
+passwords is the most common way (something they know)
+
+people are trying to get other people's passwords
+if they are easy to guess, then they are vulnerable
+if they are stored in an insecure store, or a single point of access sstore, they are vulnerable
+
+password hashing is teh answer ... store the hash, not the password.
+
+we are using bcryptjs to generate hashing in a login attempt is used to generate a hash, which is compared to the store hash for the real password
+knowing the stored hash doesn't help - if you supply the hash, it's as good as the wrong password
+
+rainbow tables are hascker's answer use alot of computing power to generate hash's for every possible password
